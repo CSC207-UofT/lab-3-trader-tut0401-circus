b809dc9a8357a596012f45f026b91bbcc78e14c3
artaru <artem.a.arutyuno@gmail.com>
2021-09-27 17:19:55 -0400

    Try #1

---
+++ src/main/java/newClass.java
@@ -0,0 +1,23 @@
+public class newClass implements Tradable, Drivable{
+
+
+    @Override
+    public int getPrice() {
+        return 0;
+    }
+
+    @Override
+    public void upgradeSpeed() {
+
+    }
+
+    @Override
+    public void downgradeSpeed() {
+
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return 0;
+    }
+}
